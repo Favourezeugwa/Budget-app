b41a3e4e543a39381694bdb3afe84d7746c5091a
Favourezeugwa <favourezeugwa@gmail.com>
2022-10-19 07:47:50 +0100

    Add associations and validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many :expenses, foreign_key: "category_id", dependent: :destroy, through: :expense_categories
+  has_many :expense_categories, foreign_key: "category_id", dependent: :destroy
+
+  validates :name , presence: true, length: { in: 3..20  }
+
 end
