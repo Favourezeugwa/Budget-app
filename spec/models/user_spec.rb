af7807d28f67d633bd7d761cc168e9bbe34ff0ef
Favourezeugwa <favourezeugwa@gmail.com>
2022-10-25 04:27:33 +0100

    Add user_spec test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+
+RSpec.describe User, type: :model do
+  subject do
+    User.new(
+      name: 'John',
+      email: 'john1@gmail.com',
+      password: '1234567'
+    )
+  end
+
+  before { subject.save }
+
+  describe 'valid attributes' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid with a name longer than 20 characters' do
+      subject.name = 'a' * 21
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe 'associations' do
+    it 'has many Categories' do
+      assc = described_class.reflect_on_association(:categories)
+      expect(assc.macro).to eq :has_many
+    end
+
+    it 'has many Expenses' do
+      assc = described_class.reflect_on_association(:expenses)
+      expect(assc.macro).to eq :has_many
+    end
+  end
+end
